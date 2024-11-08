cb4eac7a4ec6ac4c5319db355a06fd491ee135f3
Suhail Ahmad <ahmadsuhailahmad5@gmail.com>
2024-11-08 18:53:22 +0530

    Online store

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Suhail Ahhmad</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="https://www.linkedin.com/in/suhailahmad786/" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="https://github.com/suhail22a" target="_blank">GitHub</a>
+                </button>
+                
+            </div>
+        </div>`
+    );
+};
